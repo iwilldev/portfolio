53fec77421dabb0cfda502133fc1966c1724db35
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-17 18:48:46 -0300

    Criando componente Window

---
+++ src/components/Window/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import { useHistory } from 'react-router-dom';
+
+import closeButtonImg from '../../assets/img/ui/close.svg';
+
+interface WindowProps {
+  icon: string;
+  title: string;
+}
+
+const Window: React.FunctionComponent<WindowProps> = (props) => {
+
+  const [windowClasses, setWindowClasses] = useState('window-box closed');
+  const history = useHistory();
+
+  useEffect(() => {
+    setInterval(() => {
+      setWindowClasses('window-box')
+    }, 100);
+  }, []);
+
+  return (
+    <div className={windowClasses}>
+      <header>
+        <img src={props.icon} alt="Sobre Mim"/>
+        <p>{props.title}</p>
+        <img 
+          src={closeButtonImg} 
+          onClick={() => {
+            history.push("/");
+          }}
+          alt="Fechar Janela"
+        />
+      </header>
+      <section>.
+        {props.children}
+      </section>
+    </div>
+  );
+}
+
+export default Window;
