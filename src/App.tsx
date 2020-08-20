1c6be95a11980d766f702d9d1b367f6fa3c75116
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-20 07:22:37 -0300

    Reconfigurando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,21 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Resume from './pages/Resume';
-import CodeProjects from './pages/CodeProjects';
-import GameProjects from './pages/GameProjects';
-import DesignProjects from './pages/DesignProjects';
+import Portfolio from './pages/Portfolio';
 import Contacts from './pages/Contacts';
-import Links from './pages/Links';
 
 import './App.css';
 
 
 function App() {
+
   return (
       <BrowserRouter>
         <Route path="/" component={Home} exact />
         <Route path="/about-me" component={About} exact />
         <Route path="/resume" component={Resume} exact />
-        <Route path="/code-projects" component={CodeProjects} exact />
-        <Route path="/game-projects" component={GameProjects} exact />
-        <Route path="/design-projects" component={DesignProjects} exact />
+        <Route path="/portfolio" component={Portfolio} exact />
         <Route path="/contacts" component={Contacts} exact />
-        <Route path="/links" component={Links} exact />
       </BrowserRouter>
   );
 }
