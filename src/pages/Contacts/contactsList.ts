d607b5d13287eaecb96ace4c79feaa18987eead7
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-20 01:32:45 -0300

    Inserindo página de contatos

---
+++ src/pages/Contacts/contactsList.ts
@@ -0,0 +1,52 @@
+const contactList = [
+    {
+        image: '/assets/img/pages/contacts/phone.svg',
+        label: '021 4105 5344',
+        action: 'Número fixo',
+        link: 'tel:+552141055344'
+    },
+    {
+        image: '/assets/img/pages/contacts/whatsapp.svg',
+        label: '021 96544 3935',
+        action: 'WhatsApp',
+        link: 'https://wa.me/5521965443935'
+    },
+    {
+        image: '/assets/img/pages/contacts/gmail.svg',
+        label: 'euwilliamgoncalves@',
+        action: 'gmail.com',
+        link: 'mailto:euwilliamgoncalves@gmail.com'
+    },
+    {
+        image: '/assets/img/pages/contacts/instagram.svg',
+        label: '@magodocss',
+        action: 'Instagram',
+        link: 'https://www.instagram.com/magodocss'
+    },
+    {
+        image: '/assets/img/pages/contacts/linkedin.svg',
+        label: 'euwilliamgoncalves',
+        action: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/euwilliamgoncalves/'
+    },
+    {
+        image: '/assets/img/pages/contacts/codepen.svg',
+        label: 'euwilliamgoncalves',
+        action: 'CodePen',
+        link: 'https://codepen.io/euwilliamgoncalves'
+    },
+    {
+        image: '/assets/img/pages/contacts/medium.svg',
+        label: '@euwilliamgoncalves',
+        action: 'Medium',
+        link: 'https://medium.com/@euwilliamgoncalves'
+    },
+    {
+        image: '/assets/img/pages/contacts/rocketseat.svg',
+        label: 'euwilliamgoncalves',
+        action: 'Rocketseat',
+        link: 'https://app.rocketseat.com.br/me/euwilliamgoncalves'
+    }
+]
+
+export default contactList;
--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import TemplateBar from '../../components/TemplateBar';
 import Window from '../../components/Window';
 
+import contactsList from './contactsList';
+import './style.css';
+
 function Contacts() {
   return (
     <>
       <TemplateBar/>
-      <Window icon="/assets/img/icons/contact.svg" title="Contatos" />
+      <Window icon="/assets/img/icons/contact.svg" title="Contatos">
+        <div className="content">
+        <p>Olá, visitante! Se você veio do Instagram, ou de outro caminho, através do meu link de contato, pode ver meu portfólio e currículo através do <span className="red">'Botão Iniciar'</span>, na barra acima.</p>
+          <div className="contact-container">
+          { contactsList.map((item) => { 
+              return (
+                <a key={item.action} href={item.link}>
+                  <img src={item.image} alt={item.action}/>
+                  <div className="contact-description">
+                    <h1>{item.label}</h1>
+                    <p>{item.action}</p>
+                  </div>
+                </a>
+              )
+          })}
+          </div>
+        </div>
+      </Window>
     </>
   );
 }
