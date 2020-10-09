0a65ef529365f88e619c95f5b5e861c298a52443
William Gonçalves <euwilliamgoncalves@gmail.com>
2020-10-09 20:23:07 -0300

    Atualizando lista de vídeos e og pic

--- src/pages/Videos/videoList.ts
+++ src/pages/Videos/videoList.ts
@@ -1,5 +1,11 @@
 const videoList = [
     {
+        id: "02",
+        title: "menu responsivo com css, flexbox e javascript",
+        image: "https://img.youtube.com/vi/gJ21oN9jFsw/maxresdefault.jpg",
+        link: "https://www.youtube.com/watch?v=gJ21oN9jFsw"
+    },
+    {
         id: "01",
         title: "tipografia responsiva com css",
         image: "https://img.youtube.com/vi/3_RtSaU5hVY/maxresdefault.jpg",
