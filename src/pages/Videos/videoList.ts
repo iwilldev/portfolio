8cfabe2dafa35cb2623717a81e384db2ef6fecfb
William Gonçalves <euwilliamgoncalves@gmail.com>
2020-10-17 12:19:49 -0300

    Adicionando vídeo

--- src/pages/Videos/videoList.ts
+++ src/pages/Videos/videoList.ts
@@ -1,5 +1,11 @@
 const videoList = [
     {
+        id: "03",
+        title: "next level week 3",
+        image: "https://img.youtube.com/vi/5bgL8CeRrAE/maxresdefault.jpg",
+        link: "https://www.youtube.com/watch?v=5bgL8CeRrAE"
+    },
+    {
         id: "02",
         title: "menu responsivo com css, flexbox e javascript",
         image: "https://img.youtube.com/vi/gJ21oN9jFsw/maxresdefault.jpg",
